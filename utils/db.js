81190ed227c8b54e921b272b16e75941e557533e
pramod singh <pramodsingh0591@gmail.com>
2023-11-23 00:53:20 +0530

    added env

--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 const db = ()=>{
-    return mongoose.connect(`mongodb+srv://pramodsinghthakur0591:${process.env.MONGODB_PASS}@cluster0.bhci9is.mongodb.net/BooksDb`)
+    return mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASS}@cluster0.bhci9is.mongodb.net/BooksDb`)
 }
 
 export default db;
